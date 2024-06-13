def06899eafcfc16dcac1d225b329de7c2edcc3b
Carlos Nayan <carlosnayan@gmail.com>
2024-06-13 08:05:49 -0300

    fix: added styled-component library

--- src/screens/Groups.tsx
+++ src/screens/Groups.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import styled from "styled-components/native";
 
-export function Groups(){
-	return (
-		<View>
-			<Text>Groups</Text>
-		</View>
-	)
+export function Groups() {
+  return (
+    <Container>
+      <Title>Groups</Title>
+    </Container>
+  );
 }
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #000;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.Text`
+  font-size: 32px;
+  color: #fff;
+`;
